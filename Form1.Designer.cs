10e680c2de3668d53df2ab2a7e3265ba7b8375ef
Schuler, Simon <simon.schuler@kumavision.com>
2020-06-17 17:03:42 +0200

    Added menu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500,500);
+            // this.ClientSize = new System.Drawing.Size(500,500);
             // this.ClientSize = System.Drawing.Size()
             this.Text = "Vereinsdatenbank 2020";
         }
--- Form1.cs
+++ Form1.cs
@@ -12,23 +12,64 @@ namespace Datenbank
 {
     public partial class Form1 : Form
     {
-        private TextBox tbox;
-        public Form1() : this(WindowType.Init)
+        public Form1()
         {
 
-            // InitializeComponent();
-            // CenterToScreen();
-            // initSecurity();
-            // createGridView();
-            // createMenu();
-        }
-        public Form1(WindowType typus)
-        {
             InitializeComponent();
-            CenterToScreen();
+            AddMenu();
             this.WindowState = FormWindowState.Maximized;
+        }
+        private void AddMenu()
+        {
+            ToolStripButton homeBtn = new ToolStripButton("Home", Image.FromFile("./img/home.png"));
+            MenuStrip menu = new MenuStrip();
+
+            // Image home = Image.FromFile("./img/home.png");
+            // homeBtn.Image = home;
+            // homeBtn.AutoSize = true;
+            homeBtn.ImageAlign = ContentAlignment.MiddleCenter;
+            homeBtn.TextAlign = ContentAlignment.MiddleCenter;
+            homeBtn.Click += new EventHandler(ClickHomeBtn);
+
+            void ClickHomeBtn(Object sender, EventArgs e)
+            {
+                //TODO
+                System.Windows.Forms.MessageBox.Show("Home clicked");
             }
 
+            // ToolStripDropDownButton memberDropdown = new ToolStripDropDownButton("Mitglieder",);
+            ToolStripButton memberListBtn = new ToolStripButton("Mitgliederliste", Image.FromFile("./img/member.png"));
+            memberListBtn.Click += new EventHandler(ClickMemberBtn);
+
+            void ClickMemberBtn(Object sender, EventArgs e)
+            {
+                //TODO
+                System.Windows.Forms.MessageBox.Show("Member list clicked");
+            }
+
+            // memberDropdown.DropDown.Items.Add(memberListBtn);
+            // memberDropdown.DropDown.Size = memberDropdown.DropDown.PreferredSize;
+
+            ToolStripButton ledgerBtn = new ToolStripButton("Kassenbuch", Image.FromFile("./img/ledger.png"));
+            ledgerBtn.Click += new EventHandler(ClickLedgerBtn);
+
+            void ClickLedgerBtn(Object sender, EventArgs e)
+            {
+                //TODO
+                System.Windows.Forms.MessageBox.Show("ledger clicked");
+            }
+
+            menu.Items.Add(homeBtn);
+            menu.Items.Add(memberListBtn);
+            menu.Items.Add(ledgerBtn);
+            menu.AutoSize = true;
+
+
+            this.Controls.Add(menu);
+            this.MainMenuStrip = menu;
+
+
+        }
         private void createGridView()
         {
             DataGridView dgv = new DataGridView();
